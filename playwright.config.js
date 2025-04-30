4ca1dfb8a2500fda85b0f56428a49c60e6f15ba5
Aaro Paltemaa <apaltemaa@gmail.com>
2025-04-30 13:53:23 +0300

    e2e tests initialized

---
+++ playwright.config.js
@@ -0,0 +1,50 @@
+/* eslint-disable no-undef */
+// @ts-check
+import { defineConfig, devices } from "@playwright/test";
+
+/**
+ * Read environment variables from file.
+ * https://github.com/motdotla/dotenv
+ */
+// import dotenv from 'dotenv';
+// import path from 'path';
+// dotenv.config({ path: path.resolve(__dirname, '.env') });
+
+/**
+ * @see https://playwright.dev/docs/test-configuration
+ */
+export default defineConfig({
+  testDir: "./e2e-tests",
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 1 : undefined,
+  reporter: "html",
+
+  use: {
+    baseURL: "http://localhost:8080", // should match the dev server port
+    trace: "on-first-retry",
+  },
+
+  projects: [
+    {
+      name: "chromium",
+      use: { ...devices["Desktop Chrome"] },
+    },
+    {
+      name: "firefox",
+      use: { ...devices["Desktop Firefox"] },
+    },
+    {
+      name: "webkit",
+      use: { ...devices["Desktop Safari"] },
+    },
+  ],
+
+  webServer: {
+    command: "npm start",
+    port: 8080, // matches baseURL above
+    timeout: 120 * 1000,
+    reuseExistingServer: !process.env.CI,
+  },
+});
